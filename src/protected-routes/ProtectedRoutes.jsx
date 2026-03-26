2de8237da5cb878d6b31d3f8eac2384c67d72398
Maajid Ali <maajidali00003@gmail.com>
2026-03-26 15:16:10 +0530

    added security fix

--- src/protected-routes/ProtectedRoutes.jsx
+++ src/protected-routes/ProtectedRoutes.jsx
@@ -1,16 +1,19 @@
-import { Navigate } from "react-router";
-import { useUserContext } from "../context/useUserContext";
+import { Navigate, useLocation } from "react-router-dom";
+import { useAuth } from "../context/AuthContext"; 
 
-export default function ProtectedRoutes() {
-  const { user } = useUserContext();
+function ProtectedRoutes({ children }) {
+  const location = useLocation();
+  const { user, authLoading } = useAuth();
+
+  if (authLoading) {
+    return <div className="text-sm text-zinc-300">Checking session...</div>;
+  }
 
   if (!user) {
-    return (
-      <Navigate
-        to="/login"
-        replace
-        state={{ message: "Please Login to access chats!" }}
-      />
-    );
+    return <Navigate to="/login" replace state={{ from: location }} />;
   }
+
+  return children;
 }
+
+export default ProtectedRoutes;
